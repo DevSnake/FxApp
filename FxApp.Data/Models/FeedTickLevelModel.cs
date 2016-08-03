769214c17730d2fda376f8f3e35f3e38932b7b25
Proho <Proho@DESKTOP-2V210HD>
2016-08-03 11:14:01 +0300

    Правки

---
+++ FxApp.Data/Models/FeedTickLevelModel.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace FxApp.Data.Models
+{
+    [Table("FeedTickLevel")]
+
+    public class FeedTickLevelModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+    }
+}
--- FxApp.Data/Models/FeedTickModel.cs
+++ FxApp.Data/Models/FeedTickModel.cs
@@ -6,11 +6,14 @@ namespace FxApp.Data.Models
     [Table("FeedTick")]
     public class FeedTickModel
     {
-        [PrimaryKey,AutoIncrement]
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
         public string Symbol { get; set; }
 
         public DateTime TimeStamp { get; set; }
+
+        public int AskId { get; set; }
+        public int BidId { get; set; }
     }
 }
